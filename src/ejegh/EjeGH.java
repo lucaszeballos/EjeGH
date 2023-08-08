d5eca27b4f07ab0d63cbea9d4a0dbbba945e2372
santinofernandez14 <santinofernandez67@gmail.com>
2023-08-08 19:51:13 -0300

    agregue algo

--- src/ejegh/EjeGH.java
+++ src/ejegh/EjeGH.java
@@ -16,6 +16,7 @@ public class EjeGH {
      */
     public static void main(String[] args) {
         System.out.println("Linea de codigo");
+        System.out.println("hola mundo");
     }
     
 }
